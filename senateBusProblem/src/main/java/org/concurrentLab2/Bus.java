d6c85817eb6b9c5003e752c0582f41e2de26e325
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-01-17 22:12:18 +0530

    inter arrival time and comments added

--- senateBusProblem/src/main/java/org/concurrentLab2/Bus.java
+++ senateBusProblem/src/main/java/org/concurrentLab2/Bus.java
@@ -1,13 +1,23 @@
 package org.concurrentLab2;
 
+import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class Bus extends Thread {
     private int capacity = 50;
+    //keeps track of how many riders are waiting to board the bus
     private int waiting = 0;
+    //used to ensure that only one thread can access the waiting variable at a time
     private Semaphore mutex = new Semaphore(1);
+    //semaphore object that the rider threads use to wait for the bus to arrive
     private Semaphore bus = new Semaphore(0);
+    //semaphore object that the rider threads use to signal to the bus thread that they have boarded
     private Semaphore boarded = new Semaphore(0);
+
+    //use to generate inter arrival time
+    private static Random rand = new Random();
+    private static final int BUS_INTERARRIVAL_TIME = 1200000;
+
     public void depart() {
         System.out.println("Bus is departing.");
     }
@@ -15,14 +25,24 @@ public class Bus extends Thread {
     public void run() {
         while(true) {
             try {
-                Thread.sleep(50000);
+                //generate a random number that follows an exponential distribution
+                int interArrivalTime = (int) (-BUS_INTERARRIVAL_TIME * Math.log(rand.nextDouble()));
+                Thread.sleep(interArrivalTime);
+
+                //Wait for the mutex semaphore to be available
                 mutex.acquire();
+
+                // n represents the number of riders that will be allowed to board the bus
                 int n = Math.min(waiting, capacity);
+
+                // Iterate n number of times to board riders to the bus
                 for (int i = 0; i < n; i++) {
                     bus.release();
                     boarded.acquire();
                 }
+                // Decrement the waiting variable by the number of riders who boarded
                 waiting = Math.max(waiting-capacity, 0);
+
                 mutex.release();
                 depart();
             } catch (InterruptedException e) {
@@ -33,12 +53,16 @@ public class Bus extends Thread {
 
     public void waitForRider(Rider rider) {
         try {
+            // Wait for the mutex semaphore to be available
             mutex.acquire();
             waiting++;
             System.out.println("Rider "+ rider.getId() + " is waiting for the bus.");
             mutex.release();
+
+            //Wait for the bus semaphore to be signaled by the bus thread
             bus.acquire();
             rider.board();
+            //Signal the boarded semaphore to indicate to the bus thread that the rider has boarded
             boarded.release();
         } catch (InterruptedException e) {
             e.printStackTrace();
--- senateBusProblem/src/main/java/org/concurrentLab2/RiderHandler.java
+++ senateBusProblem/src/main/java/org/concurrentLab2/RiderHandler.java
@@ -1,10 +1,14 @@
 package org.concurrentLab2;
 
+import java.util.Random;
+
 public class RiderHandler extends Thread {
 
     Rider[] riders;
     Bus bus;
     int limit;
+    private static Random rand = new Random();
+    private static final int RIDER_INTERARRIVAL_TIME = 30000;
 
     public RiderHandler(Rider[] riders, Bus bus, int limit) {
         this.riders = riders;
@@ -17,7 +21,9 @@ public class RiderHandler extends Thread {
             riders[i] = new Rider(i+1, bus);
             riders[i].start();
             try {
-                Thread.sleep(1000);
+                //generate a random number that follows an exponential distribution
+                int interArrivalTime = (int) (-RIDER_INTERARRIVAL_TIME * Math.log(rand.nextDouble()));
+                Thread.sleep(interArrivalTime);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
